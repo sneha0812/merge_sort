49cde4b83612650fff9a9ddc9f4427b0bd117f4f
Sneha Agrawal <snehaagrawal@apconic.net>
2016-07-18 15:15:52 +0530

    dwq

---
+++ app.js
@@ -0,0 +1,14 @@
+    function mergeSort(array) {
+    if (array.length <= 1) {
+        return array[0];
+    }
+    let middle = array.length / 2;
+    let start = 0, end = array.length - 1;
+    console.log(array.length);
+    let left = array.split(start, middle);
+    let right = array.split(middle, end);
+    return mergeSort(mergeSort(left), mergeSort(right));
+}
+console.log(mergeSort([4, 8, 1, 5, 0, 6, 2]));
+
+
